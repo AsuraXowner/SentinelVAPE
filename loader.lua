012e9acca6b24d20f729290a8df9bc039971d379
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-06 21:21:23 +0300

    Add loader functionality for file management and downloads
    Implement file handling and downloading functions for loader.

---
+++ loader.lua
@@ -0,0 +1,49 @@
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+
+local delfile = delfile or function(file)
+	writefile(file, '')
+end
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/'..select(1, path:gsub('newvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+
+local function wipeFolder(path)
+	if not isfolder(path) then return end
+	for _, file in listfiles(path) do
+		if file:find('loader') then continue end
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+			delfile(file)
+		end
+	end
+end
+
+for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+	if not isfolder(folder) then
+		makefolder(folder)
+	end
+end
+
+if not shared.VapeDeveloper then
+	local commit = "main"
+	writefile('newvape/profiles/commit.txt', commit)
+end
+
+return loadstring(downloadFile('newvape/main.lua'), 'main')()
