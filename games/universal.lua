c03c952bea7a117cf7152d64ebf2757093c0d31a
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-07 03:26:54 +0300

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8f99ef368e4836a52dfd5bf2f962cd1a6c8e7f6dcb7e8b21eaf64fac4aa1496c/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ede545d7d6734a37c80b89e82b5ed44a78530d00420fa0a48ff0931615d4752b/download"))()
