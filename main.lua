c9d4a41828d47c5f95c49cdd5410c01101d83b11
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-07 03:44:47 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -98,7 +98,7 @@ vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
 
 if not shared.VapeIndependent then
-	if game.PlaceId==168556275 then
+	if game.PlaceId==286090429 then
 	    loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ede545d7d6734a37c80b89e82b5ed44a78530d00420fa0a48ff0931615d4752b/download"))()
 	else
 		vape:CreateNotification('Vape', 'Failed to Load Universal not supported', 30, 'alert')
