6080ef4ac98531a4bf82b89e8fc392e7a5b194db
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-10 17:01:59 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -101,7 +101,8 @@ if not shared.VapeIndependent then
 	if game.PlaceId==286090429 then
 	    loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ede545d7d6734a37c80b89e82b5ed44a78530d00420fa0a48ff0931615d4752b/download"))()
 	else
-		vape:CreateNotification('Vape', 'Failed to Load Universal not supported', 30, 'alert')
+		vape:CreateNotification('Vape', 'Unsupported Game script will run on universal mode', 10, 'warning')
+		loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/87e61dff389ed0dcd925720f380a0f4b3eb4a349de97a7cce3562b8ef3841009/download"))()
 	end
 	finishLoading()
 else
