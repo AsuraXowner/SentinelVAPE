ca64904410186ececf67b3d874d22ae2f80b2e0c
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-27 16:32:40 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -108,6 +108,7 @@ if not shared.VapeIndependent then
 		vape:CreateNotification('Vape', 'Unsupported Game script will run on universal mode', 10, 'warning')
 		loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/87e61dff389ed0dcd925720f380a0f4b3eb4a349de97a7cce3562b8ef3841009/download"))()
 	end
+	task.wait(20)
 	finishLoading()
 else
 	vape.Init = finishLoading
