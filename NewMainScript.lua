ce9dba11bb1d85e36f5ec38e0c2c3a0e5fbe36bc
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-27 16:05:53 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -5,7 +5,7 @@ local isfile = isfile or function(file)
 	return suc and res ~= nil and res ~= ''
 end
 
-if not isfile("newvape/profiles/Sentinel.cfg") then
+if isfolder("newvape") and not isfile("newvape/profiles/Sentinel.cfg") then
     delfolder("newvape")--deletes old vape stuff
 end
 
