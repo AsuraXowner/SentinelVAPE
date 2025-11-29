867af6f7bbd7da870f7536fff234710434f53a5a
Etruria Products <165428352+AsuraXowner@users.noreply.github.com>
2025-11-29 18:58:37 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
+getgenv().VapeLoaded = getgenv().VapeLoaded or false
+
 if identifyexecutor then
 	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
 		getgenv().setthreadidentity = nil
@@ -24,21 +26,23 @@ local isfile = isfile or function(file)
 	end)
 	return suc and res ~= nil and res ~= ''
 end
+
 local cloneref = cloneref or function(obj)
 	return obj
 end
-local playersService = cloneref(game:GetService('Players'))
+
+local playersService = cloneref(game:GetService("Players"))
 
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet("https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/"..select(1, path:gsub("newvape/", "")), true)
 		end)
-		if not suc or res == '404: Not Found' then
+		if not suc or res == "404: Not Found" then
 			error(res)
 		end
-		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		if path:find(".lua") then
+			res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n"..res
 		end
 		writefile(path, res)
 	end
@@ -48,6 +52,7 @@ end
 local function finishLoading()
 	vape.Init = nil
 	vape:Load()
+
 	task.spawn(function()
 		repeat
 			vape:Save()
@@ -62,13 +67,13 @@ local function finishLoading()
 			local teleportScript = [[
 				shared.vapereload = true
 				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
+					loadstring(readfile("newvape/loader.lua"), "loader")()
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/loader.lua", true), "loader")()
 				end
 			]]
 			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+				teleportScript = "shared.VapeDeveloper = true\n" .. teleportScript
 			end
 			if shared.VapeCustomProfile then
 				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
@@ -79,38 +84,47 @@ local function finishLoading()
 	end))
 
 	if not shared.vapereload then
-		if not vape.Categories then return end
-		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+		if vape.Categories and vape.Categories.Main.Options["GUI bind indicator"].Enabled then
+			vape:CreateNotification("Finished Loading", vape.VapeButton and "Press the button in the top right to open GUI" or "Press "..table.concat(vape.Keybind, " + "):upper().." to open GUI", 5)
 		end
 	end
 end
 
-if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'new')
+if not isfile("newvape/profiles/gui.txt") then
+	writefile("newvape/profiles/gui.txt", "new")
 end
-local gui = readfile('newvape/profiles/gui.txt')
 
-if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
+local gui = readfile("newvape/profiles/gui.txt")
+
+if not isfolder("newvape/assets/"..gui) then
+	makefolder("newvape/assets/"..gui)
 end
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
+
+vape = loadstring(downloadFile("newvape/guis/"..gui..".lua"), "gui")()
 shared.vape = vape
 
 if not shared.VapeIndependent then
-	if game.PlaceId==286090429 then
-	    loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ede545d7d6734a37c80b89e82b5ed44a78530d00420fa0a48ff0931615d4752b/download"))()
-	elseif game.PlaceId==17625359962 then
+	if game.PlaceId == 286090429 then
+		loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ede545d7d6734a37c80b89e82b5ed44a78530d00420fa0a48ff0931615d4752b/download"))()
+	elseif game.PlaceId == 17625359962 then
 		loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/9cf565b6746d66cda10c26de28695dcf3407c88e7aa4eed0421f5d1c27860812/download"))()
-	elseif game.PlaceId==301549746 or game.PlaceId==1480424328 or game.PlaceId==1869597719 then
-        loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/55196de3700ccd12251b6da7fd669fe4e4cebce53a4ef87d1d6282cf64f750cb/download"))()
+	elseif game.PlaceId == 301549746 or game.PlaceId == 1480424328 or game.PlaceId == 1869597719 then
+		loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/55196de3700ccd12251b6da7fd669fe4e4cebce53a4ef87d1d6282cf64f750cb/download"))()
 	else
-		vape:CreateNotification('Vape', 'Unsupported Game script will run on universal mode', 10, 'warning')
+		vape:CreateNotification("Vape", "Unsupported Game script will run on universal mode", 10, "warning")
 		loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/87e61dff389ed0dcd925720f380a0f4b3eb4a349de97a7cce3562b8ef3841009/download"))()
 	end
-	task.wait(20)
-	finishLoading()
+
+	task.spawn(function()
+		repeat task.wait() until getgenv().VapeLoaded == true
+		finishLoading()
+	end)
+
 else
-	vape.Init = finishLoading
+	vape.Init = function()
+		repeat task.wait() until getgenv().VapeLoaded == true
+		finishLoading()
+	end
+
 	return vape
 end
