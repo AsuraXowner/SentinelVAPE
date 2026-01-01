ff34ae2903684d2cb5dcd355370cca21c638ddb1
void2realyt <x2voidy09@gmail.com>
2025-12-31 19:24:06 -0500

    Add files via upload

--- guis/new.lua
+++ guis/new.lua
@@ -59,8 +59,8 @@ local tween = {
 local uipallet = {
 	Main = Color3.fromRGB(26, 25, 26),
 	Text = Color3.fromRGB(200, 200, 200),
-	Font = Font.fromEnum(Enum.Font.Cartoon),
-	FontSemiBold = Font.fromEnum(Enum.Font.Cartoon, Enum.FontWeight.SemiBold),
+	Font = Font.fromEnum(Enum.Font.Arial),
+	FontSemiBold = Font.fromEnum(Enum.Font.Arial, Enum.FontWeight.SemiBold),
 	Tween = TweenInfo.new(0.16, Enum.EasingStyle.Linear)
 }
 
@@ -161,7 +161,7 @@ end
 
 local function addCorner(parent, radius)
 	local corner = Instance.new('UICorner')
-	corner.CornerRadius = radius or UDim.new(0, 25)
+	corner.CornerRadius = radius or UDim.new(0, 5)
 	corner.Parent = parent
 
 	return corner
