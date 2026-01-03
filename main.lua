b3f32d308c573370c2d86b01726afe0ac829b77a
Sentinel Products <165428352+AsuraXowner@users.noreply.github.com>
2026-01-03 16:40:38 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,5 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/87e61dff389ed0dcd925720f380a0f4b3eb4a349de97a7cce3562b8ef3841009/download"))()
---ooga booga
+game:GetService('StarterGui'):SetCore('SendNotification', {
+					Title = 'Sentinel',
+					Text = 'Sentinel Currently down.',
+					Duration = 60,
+})
