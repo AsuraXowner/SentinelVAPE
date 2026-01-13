59ff7986c889654d4162399218a57b309826f30b
Sentinel Products <165428352+AsuraXowner@users.noreply.github.com>
2026-01-13 16:23:34 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -5,10 +5,6 @@ local isfile = isfile or function(file)
 	return suc and res ~= nil and res ~= ''
 end
 
-if isfolder("newvape") and not isfile("newvape/profiles/Sentinel.cfg") then
-    delfolder("newvape")--deletes old vape stuff
-end
-
 local delfile = delfile or function(file)
 	writefile(file, '')
 end
@@ -16,7 +12,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/' .. select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/main/' .. select(1, path:gsub('sentinelvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -39,7 +35,7 @@ local function wipeFolder(path)
 	end
 end
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+for _, folder in {'sentinelvape', 'sentinelvape/games', 'sentinelvape/profiles', 'sentinelvape/assets', 'sentinelvape/libraries', 'sentinelvape/guis'} do
 	if not isfolder(folder) then
 		makefolder(folder)
 	end
@@ -47,13 +43,13 @@ end
 
 if not shared.VapeDeveloper then
 	local commit = 'main'
-	if (not isfile('newvape/profiles/commit.txt')) or readfile('newvape/profiles/commit.txt') ~= commit then
-		wipeFolder('newvape')
-		wipeFolder('newvape/games')
-		wipeFolder('newvape/guis')
-		wipeFolder('newvape/libraries')
+	if (not isfile('sentinelvape/profiles/commit.txt')) or readfile('sentinelvape/profiles/commit.txt') ~= commit then
+		wipeFolder('sentinelvape')
+		wipeFolder('sentinelvape/games')
+		wipeFolder('sentinelvape/guis')
+		wipeFolder('sentinelvape/libraries')
 	end
-	writefile('newvape/profiles/commit.txt', commit)
+	writefile('sentinelvape/profiles/commit.txt', commit)
 end
 
-return loadstring(downloadFile('newvape/main.lua'), 'main')()
+return loadstring(downloadFile('sentinelvape/main.lua'), 'main')()
